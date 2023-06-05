2c1f986a576963dba24be953a2beb41f9db40e8b
rickypazd <rickypazd@hotmail.com>
2023-06-05 19:52:03 -0400

    Reparando que no se vehia los pack extra

--- src/Model/tapeke/pack/Action.ts
+++ src/Model/tapeke/pack/Action.ts
@@ -13,14 +13,16 @@ export default class Action extends SAction {
         var pack_extras = Model.pack_extra.Action.getAll({
             vigentes: true
         });
-        if (!packs  || !pack_extras) return null;
+        if (!packs || !pack_extras) return null;
 
         // var lista = Object.values(pedidos);
         var lista_pack_extras = Object.values(pack_extras);
+        // console.log(lista_pack_extras)
         let curday = new SDate().toString("yyyy-MM-dd");
         Object.values(packs).map((pack: any) => {
             // var pedidos_del_pack = lista.filter((obj: any) => obj.key_pack == pack.key);
-            var extras_del_pack = lista_pack_extras.filter((obj: any) => (obj.key_pack == pack.key && new SDate(obj.fecha, "yyyy-MM-ddThh:mm:ss").toString("yyyy-MM-dd") == curday))
+            var extras_del_pack = lista_pack_extras.filter((obj: any) => (obj.key_pack == pack.key ))
+            // var extras_del_pack = lista_pack_extras.filter((obj: any) => (obj.key_pack == pack.key && new SDate(obj.fecha, "yyyy-MM-ddThh:mm:ss").toString("yyyy-MM-dd") == curday))
             // pack.pedidos = pedidos_del_pack;
             // var pedidos_activos = pedidos_del_pack.filter((p: any) => p.state != "pendiente_pago" && p.state != "timeout_pago");
 
@@ -30,6 +32,7 @@ export default class Action extends SAction {
             extras_del_pack.map((o: any) => cantidad_extra += parseFloat(o.cantidad));
             pack.cantidad_disponibles = (parseFloat(pack.cantidad) + (cantidad_extra)) - cantidad
             pack.cantidad_total = parseFloat(pack.cantidad) + (cantidad_extra);
+            // console.log(cantidad_extra)
         })
         return packs;
     }
