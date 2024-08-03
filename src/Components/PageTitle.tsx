0367a7aaf2b91f4c77ff4b42760b61e541065b1d
rickypazd <35882906+rickypazd@users.noreply.github.com>
2024-08-03 02:08:32 -0400

    commir

---
+++ src/Components/PageTitle.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { SPage, SText, STheme, SView } from "servisofts-component";
+import Model from "../Model";
+
+type PageTitlePropsType = {
+    title: string,
+}
+export default class PageTitle extends React.Component<PageTitlePropsType> {
+    render() {
+        const restaurante = Model.restaurante.Action.getSelect();
+        return <SView col={"xs-12"}>
+            <SText font='Montserrat-ExtraBold' fontSize={16}>{this.props.title}</SText>
+            <SText font={"Montserrat-SemiBold"} fontSize={14} color={STheme.color.primary} >{restaurante?.nombre}</SText>
+        </SView>
+    }
+}
